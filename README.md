98325703d0d5820d1989f1e1b3c40c8bb05b4931
Shuichiro MAKIGAKI <shuichiro.makigaki@gmail.com>
2020-07-16 17:36:45 +0900

    Follow point output changes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# T3PO
+# T3PO: TSUBAME 3.0 POint checker
+
+```
+$ cat ~/.ssh/sshkey | docker container run -i --rm --env-file ~/t3po/env makisyu/t3po  
+$ crontab -l
+0 9,15 * * * cat ~/.ssh/sshkey | docker container run -i --rm --env-file ~/t3po/env makisyu/t3po
+```
+
+`env` file example:
+
+```
+SLACK_ACCESS_TOKEN=xoxp-hogehoge
+SLACK_CHANNEL_NAME=tsubame    # no '#' prefixed
+T3_LOGIN_USERNAME=user1
+T3_POINT_NOTIFY_THRESHOLD=999999
+T3_POINT_NOTIFY_IGNORED_GROUPS=group1,group2    # These groups are not notified.
+```
--- t3po.py
+++ t3po.py
@@ -1,55 +1,66 @@
 import logging
 import os
+from typing import List
 
-from slackclient import SlackClient
+from slack import WebClient
 from fabric import Connection
 
 SLACK_ACCESS_TOKEN = os.environ['SLACK_ACCESS_TOKEN']
 SLACK_CHANNEL_NAME = os.environ['SLACK_CHANNEL_NAME']
-T3_POINT_NOTIFY_THRESHOLD = int(os.getenv('T3_POINT_NOTIFY_THRESHOLD', 999999999999999))
+T3_POINT_NOTIFY_THRESHOLD = int(os.getenv('T3_POINT_NOTIFY_THRESHOLD', 999999))
+T3_POINT_NOTIFY_IGNORED_GROUPS = os.getenv('T3_POINT_NOTIFY_IGNORED_GROUPS', '').split(',')
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(levelname)s] %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
 
 
-def get_t3_point():
+class T3Point:
+    def __init__(self, gid: int, group_name: str, deposit: int, balance: int):
+        self.gid = gid
+        self.group_name = group_name
+        self.deposit = deposit
+        self.balance = balance
+
+
+def get_t3_point() -> List[T3Point]:
     try:
         result = Connection('login.t3.gsic.titech.ac.jp').run('t3-user-info group point', hide='both')
     except Exception:
-        [logging.error(l) for l in result.stderr.splitlines()]
+        logging.error(result.stderr)
         raise
-    [logging.debug(l) for l in result.stdout.splitlines()]
-    points = {}
-    for l in result.stdout.splitlines()[2:]:
+    points = []
+    for l in result.stdout.splitlines()[2:-2]:
         t = l.split()
-        points[t[1]] = {'gid': int(t[0]), 'point': int(t[2])}
+        points.append(T3Point(int(t[0]), t[1], int(t[2]), int(t[3])))
     return points
 
 
-def format_topic(points):
-    s = None
-    for group_name in points.keys():
-        point = points[group_name]['point']
-        if point < T3_POINT_NOTIFY_THRESHOLD:
-            if s is None:
-                s = ''
-            s += f'{group_name}={point:,} '
-    return s
+def format_topic(points: List[T3Point]) -> str:
+    return ' '.join([f'{p.group_name}={p.balance:,}' for p in points])
+
+
+def is_notified(point: T3Point) -> bool:
+    return point.group_name not in T3_POINT_NOTIFY_IGNORED_GROUPS \
+           and point.balance < T3_POINT_NOTIFY_THRESHOLD
 
 
 def t3po():
-    points = get_t3_point()
-    logging.info(points)
-    cli = SlackClient(SLACK_ACCESS_TOKEN)
-    result = cli.api_call('channels.list')
-    logging.debug(result) if result['ok'] else logging.error(result)
-    channel = [ch for ch in result['channels'] if ch['name'] == SLACK_CHANNEL_NAME]
-    assert len(channel) == 1
-    topic = format_topic(points)
-    if topic is None:
+    cli = WebClient(token=SLACK_ACCESS_TOKEN)
+    resp = cli.conversations_list()
+    if not resp['ok']:
+        logging.error(resp)
         return
-    result = cli.api_call('channels.setTopic', channel=channel[0]['id'], topic=topic)
-    logging.debug(result) if result['ok'] else logging.error(result)
+    channel = [_ for _ in resp['channels'] if _['name'] == SLACK_CHANNEL_NAME]
+    if len(channel) == 0:
+        logging.error('Target channel is not found')
+        logging.error(resp)
+        return
+    points = [p for p in get_t3_point() if is_notified(p)]
+    if len(points) == 0:
+        return
+    resp = cli.conversations_setTopic(channel=channel[0]['id'], topic=format_topic(points))
+    if not resp['ok']:
+        logging.error(resp)
 
 
 def main():
